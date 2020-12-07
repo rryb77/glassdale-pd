f8c00757dd29d7d2f8e9f141e87e9cb364d19975
rryb77 <rryb77@gmail.com>
2020-12-07 15:54:54 -0600

    Added the ability to save notes to the JSON server

--- scripts/notes/NoteForm.js
+++ scripts/notes/NoteForm.js
@@ -1,12 +1,47 @@
 const contentTarget = document.querySelector(".noteFormContainer")
+const eventHub = document.querySelector(".container")
+import { saveNote } from './NoteProvider.js'
+
+//Send the data to the stored db via the API
+
+// Handle browser-generated click event in component
+eventHub.addEventListener("click", clickEvent => {
+    if (clickEvent.target.id === "saveNote") {
+        
+        // Need to gather the data from the form
+        // The .value attribute captures the text typed in those respective input element
+        const author = document.querySelector("#author")
+        const text = document.querySelector("#text")
+        const suspect = document.querySelector("#suspect")
+
+
+        // Make a new object representation of a note
+        // Use the defined variables above to create key/value pairs
+        const newNote = {
+            // Key/value pairs here
+            author: author.value,
+            text: text.value,
+            suspect: suspect.value,
+            timestamp: Date.now()
+        }
+
+        // Change API state and application state
+        saveNote(newNote)
+        author.value = ""
+        text.value = ""
+        suspect.value = ""
+        
+    }
+})
 
 const render = () => {
     contentTarget.innerHTML = `
-        <input type="text" id="note-text">
-        <select class="dropdown" id="crimeSelect">
-            <option value="0">Please select a criminal...</option>
-        </select>
-            <input type="date" id="note-date" name="date">
+        <label for="author">Author:</label>
+        <input type="text" id="author" placeholder="author name">
+        <label for="text">Notes:</label>
+        <textarea id="text" placeholder="note text"></textarea>
+        <label for="suspect">Suspect:</label>
+        <input type="text" id="suspect" placeholder="suspect name">
         <button id="saveNote">Save Note</button>
     `
 }
--- scripts/notes/NoteProvider.js
+++ scripts/notes/NoteProvider.js
@@ -1,4 +1,5 @@
 const eventHub = document.querySelector(".container")
+let notes = []
 
 const dispatchStateChangeEvent = () => {
     const noteStateChangedEvent = new CustomEvent("noteStateChanged")
@@ -8,6 +9,7 @@ const dispatchStateChangeEvent = () => {
 
 const getNotes = () => {
     return fetch('http://localhost:8088/notes')
+        //Using GET by default
         .then(response => response.json())
         .then(parsedNotes => {
             notes = parsedNotes
@@ -17,10 +19,12 @@ const getNotes = () => {
 
 export const saveNote = note => {
     return fetch('http://localhost:8088/notes', {
+        //POST is used to post new data
         method: "POST",
         headers: {
             "Content-Type": "application/json"
         },
+        //Take the object passed in to saveNote and convert it to JSON via the JSON.stringify method.
         body: JSON.stringify(note)
     })
     .then(getNotes)
